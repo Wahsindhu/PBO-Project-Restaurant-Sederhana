4e30715ca699f7b8bc5cbfac39edac0d5cf7e01d
Wahsindhu <sindhuwedana@gmail.com>
2023-04-10 19:48:28 +0800

    first commit

---
+++ Main.java
@@ -0,0 +1,31 @@
+package Mystore;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String [] args) {
+        String user, password;
+        int check = 2;
+        Scanner scanner = new Scanner(System.in);
+
+        Login user1 = new Login();
+        do {
+            System.out.println("Silahkan Login");
+            System.out.println("=======LOGIN=======");
+            System.out.println("masukkan username :");user = scanner.nextLine();
+            System.out.println("masukkan password :");password = scanner.nextLine();
+            System.out.println("===================");
+
+            if (user.equals(user1.getUsername_user()) && password.equals(user1.getPassword_user()) || user.equals(user1.getUsername_admin()) && password.equals(user1.getPassword_admin())) {
+             check = 0;
+             if (user.equals(user1.getUsername_user())) {
+                 System.out.println("Selamat datang " + user);
+             } else if (user.equals(user1.getUsername_admin())) {
+                System.out.println("Selamat datang admin " + user);
+             }
+            }else{
+                System.out.println("Username atau Password anda salah");
+            }
+        }while(check > 1);
+    }
+}
