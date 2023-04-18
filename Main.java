7ef7c918471ff67f668a5ff993bea22cfc6db0b4
Wahsindhu <sindhuwedana@gmail.com>
2023-04-18 16:26:39 +0800

    Kelas ini berisikan menu login, validasi user, menu admin, dan menu customer

--- Main.java
+++ Main.java
@@ -1,86 +1,145 @@
 package Mystore;
 
-import Mystore.Login;
-import Mystore.Order;
-import Mystore.Restaurant;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    static ArrayList<Restaurant> restaurants = new ArrayList<>();
+    public Main() {
+    }
+
     public static void main(String[] args) {
+        int check = 2;
         Scanner scanner = new Scanner(System.in);
-        Restaurant restaurant = new Restaurant();
-        Order order = new Order();
-        Login login = new Login();
-        boolean loggedIn = false;
+        Login user1 = new Login();
 
-        while (!loggedIn) {
-            System.out.println("===== Login =====");
-            System.out.print("Username: ");
-            String username = scanner.next();
-            System.out.print("Password: ");
-            String password = scanner.next();
-            System.out.println("=================");
+        do {
+            System.out.println("Silahkan Login");
+            System.out.println("=======LOGIN=======");
+            System.out.print("masukkan username : ");
+            String user = scanner.nextLine();
+            System.out.print("masukkan password : ");
+            String password = scanner.nextLine();
+            System.out.println("===================");
+            if ((!user.equals(user1.getUsername_user()) || !password.equals(user1.getPassword_user())) && (!user.equals(user1.getUsername_admin()) || !password.equals(user1.getPassword_admin()))) {
+                System.out.println("Username atau Password anda salah");
+            } else {
+                check = 0;
+                if (user.equals(user1.getUsername_user())) {
+                    System.out.println("Selamat datang " + user);
+                    int customerChoice = 0;
+                    do {
+                        System.out.println("=======MENU CUSTOMER=======");
+                        System.out.println("1. Lihat Restoran");
+                        System.out.println("2. Pilih Restoran");
+                        System.out.println("3. Keluar");
+                        System.out.println("==========================");
+                        System.out.print("Pilih opsi : ");
+                        customerChoice = scanner.nextInt();
+                        scanner.nextLine();
 
-            if (login.login(username, password)) {
-                System.out.println("Welcome " + username);
-                loggedIn = true;
-                if (login.isAdmin(username)) {
-                    boolean adminMenu = true;
-                    while (adminMenu) {
-                        System.out.println("\n===== Admin Menu =====");
-                        System.out.println("1. View Restaurant");
-                        System.out.println("2. Add Restaurant");
-                        System.out.println("3. Remove Restaurant");
-                        System.out.println("4. Exit");
-                        System.out.print("Choose option: ");
-                        int adminOption = scanner.nextInt();
-                        System.out.println("======================");
-                        switch (adminOption) {
+                        switch (customerChoice) {
                             case 1:
-                                restaurant.viewRestaurant();
+                                Restaurant.showRestaurants(restaurants);
                                 break;
                             case 2:
-                                restaurant.addRestaurant();
+                                // Menampilkan daftar restoran yang tersedia
+                                Restaurant.showRestaurants(restaurants);
+
+                                // Memilih restoran
+                                System.out.print("Pilih restoran : ");
+                                String selectedRestaurant = scanner.nextLine();
+
+                                // Mencari restoran yang dipilih di dalam list restaurants
+                                Restaurant chosenRestaurant = null;
+                                for (Restaurant restaurant : restaurants) {
+                                    if (restaurant.getName().equalsIgnoreCase(selectedRestaurant)) {
+                                        chosenRestaurant = restaurant;
+                                        break;
+                                    }
+                                }
+
+                                // Jika restoran ditemukan, tampilkan menu dan pilihan untuk memesan
+                                if (chosenRestaurant != null) {
+                                    ArrayList<String> menus = chosenRestaurant.getMenus();
+                                    ArrayList<Double> prices = chosenRestaurant.getPrice();
+
+                                    System.out.println("Menu " + chosenRestaurant.getName() + ":");
+                                    for (int i = 0; i < menus.size(); i++) {
+                                        System.out.println((i+1) + ". " + menus.get(i) + " - Rp " + prices.get(i));
+                                    }
+
+                                    // Memilih menu dan menambahkannya ke keranjang belanja
+                                    ArrayList<String> cartMenus = new ArrayList<>();
+                                    ArrayList<Double> cartPrices = new ArrayList<>();
+                                    boolean continueOrdering = true;
+                                    while (continueOrdering) {
+                                        System.out.print("Pilih nomor menu: ");
+                                        int menuNumber = scanner.nextInt();
+                                        scanner.nextLine();
+                                        cartMenus.add(menus.get(menuNumber-1));
+                                        cartPrices.add(prices.get(menuNumber-1));
+
+                                        System.out.print("Tambah pesanan lagi? (y/n): ");
+                                        String answer = scanner.nextLine();
+                                        continueOrdering = answer.equalsIgnoreCase("y");
+                                    }
+
+                                    // Menampilkan daftar pesanan dan total harga
+                                    System.out.println("Pesanan Anda:");
+                                    double totalPrice = 0;
+                                    for (int i = 0; i < cartMenus.size(); i++) {
+                                        System.out.println("- " + cartMenus.get(i) + " - Rp " + cartPrices.get(i));
+                                        totalPrice += cartPrices.get(i);
+                                    }
+                                    System.out.println("Total harga: Rp " + totalPrice);
+                                } else {
+                                    System.out.println("Restoran tidak ditemukan.");
+                                }
                                 break;
                             case 3:
-                                restaurant.removeRestaurant();
+                                System.out.println("Keluar dari menu customer.");
+                                break;
+                            default:
+                                System.out.println("Pilihan tidak valid.");
+                        }
+                    } while (customerChoice != 3);
+                } else if (user.equals(user1.getUsername_admin())) {
+                    System.out.println("Selamat datang admin " + user);
+                    int choice = 0;
+                    do {
+                        System.out.println("=======MENU=======");
+                        System.out.println("1. Tambah Restoran");
+                        System.out.println("2. Hapus Restoran");
+                        System.out.println("3. Lihat Restoran");
+                        System.out.println("4. Keluar");
+                        System.out.println("===================");
+                        System.out.print("Pilih opsi : ");
+                        choice = scanner.nextInt();
+                        scanner.nextLine();
+
+                        switch (choice) {
+                            case 1:
+                                Restaurant.addRestaurant(restaurants);
+                                break;
+                            case 2:
+                                Restaurant.deleteRestaurant(restaurants);
+                                break;
+                            case 3:
+                                Restaurant.showRestaurants(restaurants);
                                 break;
                             case 4:
-                                adminMenu = false;
+                                System.out.println("Keluar dari menu admin.");
+                                check = 2;
                                 break;
                             default:
-                                System.out.println("Invalid option, please try again!");
+                                System.out.println("Pilihan tidak valid.");
                         }
-                    }
-                } else {
-                    boolean customerMenu = true;
-                    while (customerMenu) {
-                        System.out.println("\n===== Customer Menu =====");
-                        System.out.println("1. View Menu");
-                        System.out.println("2. Order");
-                        System.out.println("3. Exit");
-                        System.out.print("Choose option: ");
-                        int customerOption = scanner.nextInt();
-                        System.out.println("=========================");
-                        switch (customerOption) {
-                            case 1:
-                                restaurant.viewMenu();
-                                break;
-                            case 2:
-                                order.placeOrder();
-                                break;
-                            case 3:
-                                customerMenu = false;
-                                break;
-                            default:
-                                System.out.println("Invalid option, please try again!");
-                        }
-                    }
+                    } while (choice != 4);
                 }
-            } else {
-                System.out.println("Invalid username or password!");
             }
-        }
+        } while(check > 1);
+
     }
 }
---
+++ Pesanan.java
@@ -0,0 +1,2 @@
+package Mystore;public class Pesanan {
+}
